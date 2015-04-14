68d03700655d3a2c461b143cdcb3ea9a073157b6
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-04-14 23:21:49 +0000

    boolean functions completed

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,5 +1,8 @@
+var formLetter;
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  
+  //combine the three arguments into a single string with additional greetings and new lines
+  return "Hello Rob,\n\nTake care.\n\nSincerely,\nJoe";
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,25 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return (one !== two ) || (one != two);
+  //return true unless the two arguments are the same type (===) and are equal (==) to each other
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return (lower <= middle) && (middle <= upper);
+//return a boolean that tells us whether middle is in between upper and lower
 };
 
 module.exports.outsideRanges = function(number) {
-
+  return (number < 2 || number > 5) && (number < 10 || number > 20) && (number < 43 || number > 75);
+ // return (number < 10 || number > 20) || (number < 42);
+//  return (number < 42 || number > 75) && (number != 75);
+  //return (number < 0 || number > 6);
 };
 
 module.exports.nameAndPrice = function(name, price) {
+  return (name === "LATimes" || name === "NYTimes") && (price >= 1);
 
 };
